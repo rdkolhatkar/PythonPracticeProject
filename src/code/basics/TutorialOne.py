6b00c5d7b810f63cbcb254b595cf7fcae7b3de46
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-08-03 17:28:09 +0530

    Python Tutorial One

---
+++ src/code/basics/TutorialOne.py
@@ -0,0 +1,6 @@
+# This is a Python script that prints messages to the console.
+# To run the "TutorialOne.py"file use command :  python src/code/basics/TutorialOne.py
+print("Hello World!")
+print("This is the main module of the code.")
+print("How are you doing today?")
+print(2)
--- src/code/main.py
+++
@@ -1,2 +0,0 @@
-print("Hello World!")
-print("This is the main module of the code.");
